9b689929f47b1e7eed1944a0e8b7ad2e10f5df08
somacoder <gkbartleyjr@gmail.com>
2023-03-20 22:07:03 -0400

    -renamed build batch file

--- build.bat
+++
@@ -1,2 +0,0 @@
-conda env create --file .\environment.yml
-curl -o pix2pix.py https://raw.githubusercontent.com/affinelayer/pix2pix-tensorflow/master/pix2pix.py
---
+++ setup.bat
@@ -0,0 +1,2 @@
+CALL conda env create --file .\environment.yml
+CALL curl -o pix2pix.py https://raw.githubusercontent.com/affinelayer/pix2pix-tensorflow/master/pix2pix.py
